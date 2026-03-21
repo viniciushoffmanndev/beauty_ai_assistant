796747d8ee73238dba9fb89aad665db24cc95b69
viniciushoffmanndev <viniciushoffmanndev@users.noreply.github.com>
2026-03-21 19:08:26 -0300

    feat(products): add flavor and target fields to Product model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class Product(models.Model):
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     price = models.FloatField()
     description = models.TextField()
+
+    flavor = models.CharField(max_length=50, null=True, blank=True)
+    target = models.CharField(max_length=50, null=True, blank=True)
+
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
