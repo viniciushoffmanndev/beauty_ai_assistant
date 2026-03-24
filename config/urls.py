0ca9187b65232d12fcafdff777b5a4c16060ee67
viniciushoffmanndev <viniciushoffmanndev@users.noreply.github.com>
2026-03-24 00:19:56 -0300

    chore(config): include products app urls

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/products/', include('apps.products.urls'))
+    path('api/products/', include('apps.products.urls')),
 ]
