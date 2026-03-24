ef224b558aaaec6423089c8fabe40b66f6449dd9
viniciushoffmanndev <viniciushoffmanndev@users.noreply.github.com>
2026-03-24 00:19:24 -0300

    chore(products): add webhook route to urls

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import search_products
+from .views import search_products, whatsapp_webhook
 
 urlpatterns = [
-    path('search/', search_products, name="search_products")
+    path('search/', search_products, name="search_products"),
+    path("webhook/", whatsapp_webhook, name="webhook"),
 ]
