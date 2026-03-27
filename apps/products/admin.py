eff5a7c8d93e80aacd311e96dcbdb8a6d2fbeb20
viniciushoffmanndev <viniciushoffmanndev@users.noreply.github.com>
2026-03-27 18:08:30 -0300

    feat(products): add image preview support to Django admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Product
+from django.utils.html import format_html
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -12,6 +13,13 @@ class ProductAdmin(admin.ModelAdmin):
         "review_count",
         "is_active",
         "scraped_at",
+        "image_tag",
     )
     search_fields = ("name", "description", "brand")
     list_filter = ("brand", "is_active")
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" height="50" />', obj.image)
+        return "-"
+    image_tag.short_description = "Image"
